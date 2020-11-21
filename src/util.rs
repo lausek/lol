b8f294a8842fae841ba2b44c02266d0fcb000816
lausek <root@lausek.eu>
2020-11-21 15:16:47 +0100

    make module meta absolute

--- src/util.rs
+++ src/util.rs
@@ -3,7 +3,7 @@ macro_rules! create_lol_runtime {
     ($name:expr, $src:expr) => {{
         let mut int = lol::Interpreter::new();
         let mut trans = lol::Transpiler::new();
-        let meta: ModuleMeta = $name.to_string().into();
+        let meta: lovm2::module::meta::ModuleMeta = $name.to_string().into();
         let module = trans.build(meta, $src).unwrap();
 
         println!("{}", module);
