aeb73d3773b3f4dbcdd8ddbb5855f4457257eee8
lausek <root@lausek.eu>
2020-11-21 15:23:31 +0100

    split runtime creation macro

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,18 @@
 #[macro_export]
-macro_rules! create_lol_runtime {
+macro_rules! create_lol_module {
     ($name:expr, $src:expr) => {{
-        let mut int = lol::Interpreter::new();
         let mut trans = lol::Transpiler::new();
         let meta: lovm2::module::meta::ModuleMeta = $name.to_string().into();
         let module = trans.build(meta, $src).unwrap();
+        module
+    }};
+}
+
+#[macro_export]
+macro_rules! create_lol_runtime {
+    ($name:expr, $src:expr) => {{
+        let mut int = lol::Interpreter::new();
+        let module = lol::create_lol_module!($name, $src);
 
         println!("{}", module);
 
